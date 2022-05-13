2db5e5536736f5034bf5c0913d94483d212c8be2
GearUser <105487991+GearUser@users.noreply.github.com>
2022-05-13 18:05:54 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ class Config(object):
     APP_ID = "3845818"
     API_HASH = "95937bcf6bc0938f263fc7ad96959c6d"
     LOG_CHANNEL = -1001763491746
-    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
+    UPDATES_CHANNEL = None # Without `@` LOL
     # array to store the channel ID who are authorized to use the bot
     # dont u fucking remove this id 😤
     TG_BOT_TOKEN = "5302929820:AAEsKXElRmQ1vcwYwRYmQIYstnKPzLQF0-w"
@@ -23,12 +23,12 @@ class Config(object):
     DEF_THUMB_NAIL_VID_S = "https://placehold.it/90x90"
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = get_config("HTTP_PROXY", None)
+    HTTP_PROXY = None
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # add config vars for the display progress
-    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "▣")
-    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "▢")
+    FINISHED_PROGRESS_STR = "▣"
+    UN_FINISHED_PROGRESS_STR = "▢"
     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
       # because, https://t.me/c/1494623325/5603
-    SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
+    SHOULD_USE_BUTTONS = False
